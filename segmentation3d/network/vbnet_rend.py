Fix a bug in training. It was caused by missing a softmax layer.

segmentation3d/network/vbnet_rend.py
@@ -72,10 +72,16 @@ class SegmentationNet(nn.Module):
 
         out = self.out_block(r2_out64)
 
-        return out, (l1_out16, r2_out64, r3_out128, r4_out256)
+        # multi_layer_features = (l1_out16, r2_out64, r3_out128, r4_out256)
+        multi_layer_features = (l1_out16, r2_out64)
+
+        return out, multi_layer_features
 
     def max_stride(self):
         return 16
+
+    def num_multi_layer_features(self):
+        return 80
 
 
 class VoxelHead(nn.Module):
@@ -98,4 +104,4 @@ class VoxelHead(nn.Module):
         for layer in self.fc_layers:
             x = torch.relu(layer(x))
             x = torch.cat((x, coarse_features), dim=1)
-        return self.predictor(x)
+        return torch.softmax(self.predictor(x), dim=1)