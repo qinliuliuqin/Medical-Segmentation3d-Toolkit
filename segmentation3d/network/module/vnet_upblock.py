cbef10414bf9ed4e6d3865e6ffe98cbc54632548
Qin Liu <qinliu@med.unc.edu>
2020-03-04 17:06:00 -0500

    Set group = 1 in group normalization.

--- segmentation3d/network/module/vnet_upblock.py
+++ segmentation3d/network/module/vnet_upblock.py
@@ -9,7 +9,7 @@ class UpBlock(nn.Module):
   def __init__(self, in_channels, out_channels, num_convs, compression=False, ratio=4):
     super(UpBlock, self).__init__()
     self.up_conv = nn.ConvTranspose3d(in_channels, out_channels // 2, kernel_size=2, stride=2, groups=1)
-    self.up_gn = nn.GroupNorm(out_channels // 4, out_channels // 2)
+    self.up_gn = nn.GroupNorm(1, out_channels // 2)
     self.up_act = nn.ReLU(inplace=True)
     if compression:
       self.rblock = BottResidualBlock3(out_channels, 3, 1, 1, ratio, num_convs)
