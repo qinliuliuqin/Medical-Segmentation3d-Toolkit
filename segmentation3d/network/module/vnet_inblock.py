edaf7ac77332b0204157d5a77ea5b2a8cf6b743d
Qin Liu <qinliu@med.unc.edu>
2020-03-21 15:28:38 -0400

    Add a gpu memory-efficient variant of V-Net by replacing concatenating operations with adding operations.

--- segmentation3d/network/module/vnet_inblock.py
+++ segmentation3d/network/module/vnet_inblock.py
@@ -4,9 +4,9 @@ import torch.nn as nn
 class InputBlock(nn.Module):
   """ input block of vb-net """
 
-  def __init__(self, in_channels, out_channels):
+  def __init__(self, in_channels, out_channels, padding=1, dilation=1):
     super(InputBlock, self).__init__()
-    self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
+    self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=padding, dilation=dilation)
     self.gn = nn.GroupNorm(1, num_channels=out_channels)
     self.act = nn.ReLU(inplace=True)
 
--- segmentation3d/network/module/vnet_upblock.py
+++ segmentation3d/network/module/vnet_upblock.py
@@ -8,8 +8,8 @@ class UpBlock(nn.Module):
 
   def __init__(self, in_channels, out_channels, num_convs, compression=False, ratio=4):
     super(UpBlock, self).__init__()
-    self.up_conv = nn.ConvTranspose3d(in_channels, out_channels // 2, kernel_size=2, stride=2, groups=1)
-    self.up_gn = nn.GroupNorm(1, out_channels // 2)
+    self.up_conv = nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2, groups=1)
+    self.up_gn = nn.GroupNorm(1, out_channels)
     self.up_act = nn.ReLU(inplace=True)
     if compression:
       self.rblock = BottResidualBlock3(out_channels, 3, 1, 1, ratio, num_convs)
@@ -18,6 +18,6 @@ class UpBlock(nn.Module):
 
   def forward(self, input, skip):
     out = self.up_act(self.up_gn(self.up_conv(input)))
-    out = torch.cat((out, skip), 1)
+    out = torch.div(torch.add(out, skip), 2.0)
     out = self.rblock(out)
     return out
