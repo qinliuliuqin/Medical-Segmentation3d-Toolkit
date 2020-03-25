f573c392b60f957818ffdbb4b33fb60d93979c5f
Qin Liu <qinliu19@denton.med.unc.edu>
2020-03-25 17:17:08 -0400

    Modify path

--- segmentation3d/seg_infer.py
+++ segmentation3d/seg_infer.py
@@ -330,8 +330,8 @@ def main():
                        '3. A folder containing all testing images\n'
 
     default_input = '/shenlab/lab_stor6/qinliu/CT_Dental/datasets/test.txt'
-    default_model = '/shenlab/lab_stor6/qinliu/CT_Dental/models/model_0321_2020/model1_contrast'
-    default_output = '/shenlab/lab_stor6/qinliu/CT_Dental/results/model_0321_2020/model1_contrast'
+    default_model = '/shenlab/lab_stor6/qinliu/CT_Dental/models/model_0305_2020/model1_groupnorm_0.4_contrast'
+    default_output = '/shenlab/lab_stor6/qinliu/CT_Dental/results/model_0305_2020/model1_groupnorm_0.4_contrast'
     default_seg_name = 'seg.mha'
     default_gpu_id =7
 
