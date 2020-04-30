60df6413d12a84ebb42725597193366dae561b07
Qin Liu <sjtuliuqin@163.com>
2020-04-30 17:12:15 -0400

    Modify the interface

--- segmentation3d/seg_train.py
+++ segmentation3d/seg_train.py
@@ -1,12 +1,9 @@
 import argparse
-import os
 
 from segmentation3d.core.seg_train import train
 
 
 def main():
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = '4,5,6'
 
     long_description = "Training engine for 3d medical image segmentation"
     parser = argparse.ArgumentParser(description=long_description)
@@ -15,7 +12,6 @@ def main():
                         default='./config/train_config.py',
                         help='configure file for medical image segmentation training.')
     args = parser.parse_args()
-
     train(args.input)
 
 
