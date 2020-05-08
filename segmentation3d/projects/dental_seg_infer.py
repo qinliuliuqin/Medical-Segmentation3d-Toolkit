16cafdb3ecce63a5068feb3ffd2938bf4853b44a
qinliuliuqin <57234075+qinliuliuqin@users.noreply.github.com>
2020-05-08 12:50:23 -0400

    Update dental_seg_infer.py

--- segmentation3d/projects/dental_seg_infer.py
+++ segmentation3d/projects/dental_seg_infer.py
@@ -7,9 +7,12 @@ def dental_segmentation(input_folder, model_folder, gpu_id):
     :param input_folder:        The input dicom folder
     :param model_folder:        The folder contains trained models
     :param gpu_id:              Which gpu to use, by default, 0
-    :return: None
+    :return: A list containing binary masks of mandible and midface-both are in SimpleITK Image type.
     """
     mask = segmentation(input_folder, model_folder, '', '', gpu_id, True, False, False, False)
 
     # mask transformation-convert multi-class masks to binary mask with specified label
+    binary_masks = []
     # TO BE DONE
+       
+    return binary_masks
