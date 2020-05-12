33a302421dbcac71a840b51ba3bfe2a35535f6b5
Qin Liu <qinliu19@denton.med.unc.edu>
2020-05-11 23:13:08 -0400

    Update for training file

--- segmentation3d/seg_train.py
+++ segmentation3d/seg_train.py
@@ -5,11 +5,14 @@ from segmentation3d.core.seg_train import train
 
 def main():
 
+    import os
+    os.environ['CUDA_VISIBLE_DEVICES'] = '2,4,6'
+
     long_description = "Training engine for 3d medical image segmentation"
     parser = argparse.ArgumentParser(description=long_description)
 
     parser.add_argument('-i', '--input',
-                        default='./config/train_config.py',
+                        default='/home/qinliu19/projects/Medical-Segmentation3d-Toolkit/segmentation3d/config/train_config.py',
                         help='configure file for medical image segmentation training.')
     args = parser.parse_args()
     train(args.input)
