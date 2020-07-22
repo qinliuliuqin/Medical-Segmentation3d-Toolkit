725aa598d41f894dc58885de77f391519828222f
Qin Liu <sjtuliuqin@163.com>
2020-07-22 11:31:30 -0400

    Update color configuration files

--- segmentation3d/vis/vtk_rendering_test.py
+++ segmentation3d/vis/vtk_rendering_test.py
@@ -8,7 +8,11 @@ def test_vtk_surface_render():
     color_config_path = "/home/ql/projects/Medical-Segmentation3d-Toolkit/segmentation3d/vis/color_config.csv"
     color_dict = get_color_dict(color_config_path)
 
-    mask_folder = '/mnt/projects/CT_Dental/results/model_0305_2020/Pre_Post_Facial_Data-Ma/'
+    mask_folder = '/mnt/projects/CT_Dental/results/model_0609_2020/Pre_Post_Facial_Data-Ma_3labels'
+    picture_save_folder = '/mnt/projects/CT_Dental/results/model_0609_2020/rendering/Pre_Post_Facial_Data-Ma_3labels/bones'
+    if not os.path.isdir(picture_save_folder):
+        os.makedirs(picture_save_folder)
+
     folders = os.listdir(mask_folder)
     for folder in folders:
         if os.path.isdir(os.path.join(mask_folder, folder)):
@@ -16,7 +20,7 @@ def test_vtk_surface_render():
             mask_path = os.path.join(mask_folder, folder, 'seg.mha')
             mask = sitk.ReadImage(mask_path)
 
-            figure_save_path = os.path.join(mask_folder, '{}.png'.format(folder))
+            figure_save_path = os.path.join(picture_save_folder, '{}.png'.format(folder))
             vtk_surface_rendering(mask, color_dict, [800, 800], figure_save_path)
 
 
