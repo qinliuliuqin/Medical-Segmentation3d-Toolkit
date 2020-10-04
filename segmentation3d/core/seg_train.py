c134cf3d480c108ab8bddfea8724ba85a1d63493
Qin Liu <sjtuliuqin@163.com>
2020-10-03 22:34:44 -0400

    Fix a bug

--- segmentation3d/core/seg_train.py
+++ segmentation3d/core/seg_train.py
@@ -133,8 +133,8 @@ def train_one_epoch(net, data_loader, data_loader_m, loss_funces, opt, logger, e
         # print training loss per batch
         if use_ul:
             msg = 'epoch: {}, batch: {}, train_loss: {:.4f}, {:.4f}, {:.4f}, time: {:.4f} s/vol'
-            msg = msg.format(epoch_idx, batch_idx, train_loss.item() - train_loss_m.item(), train_loss_m.item(),
-                             train_loss_mn.item(), batch_duration)
+            msg = msg.format(epoch_idx, batch_idx, train_loss.item() - train_loss_m.item() - train_loss_mn.item(),
+                             train_loss_m.item(), train_loss_mn.item(), batch_duration)
         else:
             msg = 'epoch: {}, batch: {}, train_loss: {:.4f}, time: {:.4f} s/vol'
             msg = msg.format(epoch_idx, batch_idx, train_loss.item(), batch_duration)
