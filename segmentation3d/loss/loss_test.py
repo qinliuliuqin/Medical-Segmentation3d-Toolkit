b45ab38d14a890986ccbb39164144ea432383396
Qin Liu <sjtuliuqin@163.com>
2020-10-09 16:57:00 -0400

    Modify

--- segmentation3d/loss/loss_test.py
+++ segmentation3d/loss/loss_test.py
@@ -4,19 +4,28 @@ from segmentation3d.loss.cross_entropy_loss import CrossEntropyLoss
 from segmentation3d.loss.entropy_minization import EntropyMinimizationLoss
 
 torch.manual_seed(0)
-fps = torch.rand(1, 2, 2, 2, 2)
+fps = torch.rand(1, 2, 3, 3, 3)
+
+val, mask = fps.max(dim=1)
+idx = val > 0.5
+
+mask_valid = mask[idx]
+val_valid = fps[:, :, idx[0,:]]
+
+print(mask_valid, val_valid)
+print(mask_valid.shape, val_valid.shape)
+
+
 # fps.requires_grad = True
-#
 # probs = torch.nn.Softmax(dim=1)(fps)
 # vals, labels = probs.max(dim=1)
 # print(vals.requires_grad, labels.requires_grad)
-#
 # index = vals.ge(0.8)
 # vals_valid = vals[index]
 # labels_valid = labels[index]
-#
 # loss_func_ce = CrossEntropyLoss()
 # loss_val_ce = loss_func_ce(probs, labels)
 # print(probs, vals, labels, loss_val_ce)
-fps_n = torch.randn_like(fps)
-print(EntropyMinimizationLoss()(fps, fps))
+# fps_n = torch.rand_like(fps)
+# print(fps_n)
+# print(EntropyMinimizationLoss()(fps, fps))
--- segmentation3d/vis/vtk_rendering_test.py
+++ segmentation3d/vis/vtk_rendering_test.py
@@ -8,8 +8,10 @@ def test_vtk_surface_render():
     color_config_path = "/home/ql/projects/Medical-Segmentation3d-Toolkit/segmentation3d/vis/color_config.csv"
     color_dict = get_color_dict(color_config_path)
 
-    mask_folder = '/mnt/projects/CT_Dental/results/model_0609_2020/Pre_Post_Facial_Data-Ma_3labels'
-    picture_save_folder = '/mnt/projects/CT_Dental/results/model_0609_2020/rendering/Pre_Post_Facial_Data-Ma_3labels/bones'
+    #mask_folder = '/mnt/projects/CT_Dental/results/model_0609_2020/Pre_Post_Facial_Data-Ma_3labels'
+    mask_folder = '/mnt/projects/CT_Dental/results/teeth'
+    #picture_save_folder = '/mnt/projects/CT_Dental/results/model_0609_2020/rendering/Pre_Post_Facial_Data-Ma_3labels/bones'
+    picture_save_folder = '/mnt/projects/CT_Dental/results/teeth_rendering'
     if not os.path.isdir(picture_save_folder):
         os.makedirs(picture_save_folder)
 
@@ -17,7 +19,7 @@ def test_vtk_surface_render():
     for folder in folders:
         if os.path.isdir(os.path.join(mask_folder, folder)):
             print(folder)
-            mask_path = os.path.join(mask_folder, folder, 'seg.mha')
+            mask_path = os.path.join(mask_folder, folder, 'seg.nii.gz')
             mask = sitk.ReadImage(mask_path)
 
             figure_save_path = os.path.join(picture_save_folder, '{}.png'.format(folder))
