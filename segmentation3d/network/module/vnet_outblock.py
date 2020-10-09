6995507a1ae07d1e8ef390da79bbfe2affba6107
Qin Liu <sjtuliuqin@163.com>
2020-10-08 22:37:26 -0400

    Update module

--- segmentation3d/network/module/vnet_outblock.py
+++ segmentation3d/network/module/vnet_outblock.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 
+from segmentation3d.projects.center_based_mapping import center_based_mapping_layer
+
 
 class OutputBlock(nn.Module):
   """ output block of v-net
@@ -14,6 +16,30 @@ class OutputBlock(nn.Module):
     self.gn1 = nn.GroupNorm(1, out_channels)
     self.act1 = nn.ReLU(inplace=True)
     self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=1)
+    self.gn2 = nn.GroupNorm(1, out_channels)
+    self.softmax = nn.Softmax(dim=1)
+
+  def forward(self, input):
+    out = self.act1(self.gn1(self.conv1(input)))
+    out = self.gn2(self.conv2(out))
+    out = self.softmax(out)
+    return out
+
+
+class OutputBlock_center_based(nn.Module):
+  """ output block of v-net
+
+      The output is a list of foreground-background probability vectors.
+      The length of the list equals to the number of voxels in the volume
+  """
+
+  def __init__(self, in_channels, out_channels):
+    super(OutputBlock_center_based, self).__init__()
+    self.conv1 = center_based_mapping_layer(in_channels, out_channels)
+    self.gn1 = nn.GroupNorm(1, out_channels)
+    self.act1 = nn.ReLU(inplace=True)
+
+    self.conv2 = center_based_mapping_layer(out_channels, out_channels)
     self.gn2 = nn.GroupNorm(1, out_channels)
     self.softmax = nn.Softmax(dim=1)
 
